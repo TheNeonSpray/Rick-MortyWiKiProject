3588aac293adfc122a632090888780bb1a612283
TheNeonSpray <alexandraeb20@gmail.com>
2022-01-18 23:21:16 -0600

    Added the components of the project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import React, {useState, useEffect} from "react";
+import { Cards } from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-light bg-dark">
-        <div className="container-fluid justify-content-center">
-          <a className="title-page text-border text-decoration-none" href="#">
-          Rick & Morty Universe
-          </a>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters/>
+          </div>
+          <div className="col-8">
+            <Cards/>
+          </div>
         </div>
-      </nav>
-      <h1 className="">
-        
-      </h1>
+      </div>
     </div>
   );
 }
