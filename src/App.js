f8505b9beaa2f4fd14cd6f99286a43a0517f73c6
TheNeonSpray <alexandraeb20@gmail.com>
2022-01-18 21:59:06 -0600

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-light bg-dark">
+        <div className="container-fluid justify-content-center">
+          <a className="title-page text-border text-decoration-none" href="#">
+          Rick & Morty Universe
+          </a>
+        </div>
+      </nav>
+      <h1 className="">
+        
+      </h1>
     </div>
   );
 }
